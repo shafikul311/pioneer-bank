65d37a5dc509115f39e0dbcaefcee08a83a545a5
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2021-01-27 19:58:01 +0600

    pioner bank

--- index.js
+++ index.js
@@ -32,10 +32,12 @@ depositBtn.addEventListener("click", function(){
 
 const withdrawBtn = document.getElementById("addWithdraw");
 withdrawBtn.addEventListener("click", function(){
-    const withdrawAmount = getInputNumber("withdrawAmount");
+    const withdrawNumber = getInputNumber("withdrawAmount");
+    updateSpanText("currentWithdraw",withdrawNumber);
+    updateSpanText("currentBalance",-1*withdrawNumber)
 //     document.getElementById("withdrawAmount").value;
 //   const withdrawNumber = parseFloat(withdrawAmount);
-console.log(withdrawAmount)
+document.getElementById("withdrawAmount").value = " ";
   
 
 })
